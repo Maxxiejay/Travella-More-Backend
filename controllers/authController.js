c088a56079fd4627b5c9f05ef6f48c68710c7cc6
Maxxiejay <joshuaademax@gmail.com>
2025-04-24 19:19:31 +0100

    Silly me

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,10 +105,6 @@ exports.signin = async (req, res, next) => {
     
     // Find user by email
     const user = await userModel.findByEmail(email);
-    return res.json({
-      password: password, 
-      user: user
-    }) 
     if (!user) {
       console.error(`Signin error: User with email ${email} not found`);
       return res.status(401).json({
